cfeba68c82e5136cc29bf1bedc039ecc1696734d
Adrian <adrianmjaubert@gmail.com>
2017-05-26 20:59:24 +0100

    Añadidos dos modelos de prueba, intentando aumentar los FPS

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Pointer : MonoBehaviour {
+
+	public Text titulo;
+	public Text texto;
+	private List<string> colisiones;
+	// Use this for initialization
+	void Start () {
+		colisiones = new List<string> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision col){
+//		if (col.gameObject.tag == "Fosiles") {
+//			Debug.Log ("Nombre: " + col.gameObject.name);
+//		}
+
+	}
+	void OnCollisionStay(Collision col){
+//		if (col.gameObject.tag == "Fosiles") {
+//
+//			if(!colisiones.Contains(col.gameObject.name)){
+//				colisiones.Add(col.gameObject.name);
+//			}
+//			Debug.Log (colisiones.Count);
+//			Debug.Log ("ESTA COLISIONANDO CON: " + col.gameObject.name);
+//		}
+	}
+}
