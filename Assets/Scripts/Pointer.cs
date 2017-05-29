c72386a5d7b3d37263bd3de0a64aaea3592be803
Adrian <adrianmjaubert@gmail.com>
2017-05-29 18:59:16 +0100

    Mejorando el sistema de mostrar  la información. Funcionando en pc, arreglandolo para android

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -7,17 +7,35 @@ public class Pointer : MonoBehaviour {
 	public GameObject direccion;
 	private RaycastHit hit;
 	public float distance = 1000;
+	public GameObject botones;
+	public GameObject panel;
+	public Text titulo;
+	public Text texto;
 	// Use this for initialization
 	void Start () {
 	}
-	
+
+	void setPanel(){
+		TextAsset level = Resources.Load<TextAsset> (Application.persistentDataPath +"Textos/Persististrombus_latus.txt");
+		 
+
+		titulo.text = level.text;
+		//texto.text = text[1];
+	}
 	// Update is called once per frame
 	void Update () {
+		
 		if (Physics.Raycast (transform.position, (direccion.transform.position - transform.position), out hit, distance)) {
 			if (hit.transform.tag == "Fosiles") {
-				Debug.Log ("FOSIL TE VEO");
+				botones.active =true;
+				if (Input.GetKeyDown("joystick button 0")) {
+					setPanel ();
+					panel.active =true;       
+				}
+
 			} else {
-				Debug.Log (hit.transform.name);
+				botones.active = false;
+
 			}
 		}
 	}
